21ac5d9faf9f16dfd4260bb52f43cfa0e93216f3
Vladislav <alienv661@gmail.com>
2019-05-04 23:16:24 +0300

    code refactor

--- core/mongodb.py
+++ core/mongodb.py
@@ -1,9 +1,11 @@
 from pymongo import MongoClient
+import gridfs
 
 
 def create_db(ru_codes):
     '''This method creates and insert document with station codes'''
     client = MongoClient('localhost', 27017)
-    db = client.codes
-    db.insert_one(ru_codes)
-    print(db.find_one())
+    db = client['codes']
+    codes = db.codes
+    codes.insert_one(ru_codes)
+    print(db.insert_one(ru_codes).inserted_id)
